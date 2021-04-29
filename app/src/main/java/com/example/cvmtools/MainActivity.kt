99f951866c9cbc08557baddb51269e760317d80c
Juan Martinez <juanm.jm49@gmail.com>
2021-04-28 20:16:48 -0700

    fixed back stack

--- app/src/main/java/com/example/cvmtools/MainActivity.kt
+++ app/src/main/java/com/example/cvmtools/MainActivity.kt
@@ -39,4 +39,8 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 }
